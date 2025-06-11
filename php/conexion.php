fc169bc041f934fecc930f4304a89766cf89a2ca
LuisTona <luisalbertoag21@gmail.com>
2025-06-11 15:50:50 +0200

    Final del proyecto

--- php/conexion.php
+++ php/conexion.php
@@ -3,13 +3,13 @@
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-    
+
     Class Conexion extends mysqli {
         private $host = 'localhost';
         private $db = 'goHabits';
-        private $user = 'root';
-        private $pass = '';
-    
+        private $user = 'goHabits';
+        private $pass = 'adminHabits';
+
         public function __construct(){
             try{
                 parent::__construct($this->host,$this->user,$this->pass,$this->db);
@@ -20,5 +20,5 @@
         }
     }
 
-    
+
 ?>
